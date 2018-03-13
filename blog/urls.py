306caa5e9425e4bcff2fd66556f51ce5e6a354e8
Andrew Shaw <35270950+andrewshaw15@users.noreply.github.com>
2018-03-12 19:43:01 -0600

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,10 @@ urlpatterns = [
     path('main/', views.main, name='main'),
     path('bio/', views.bio, name='bio'),
     path('tech-tips/', views.techTips, name='tech tips'),
+    path('star-wars/', views.starWars, name='star wars'),
+    path('air-force/', views.airForce, name='air force'),
+    path('usu/', views.usu, name='usu'),
+    path('common-git-commands/', views.commonGitCommands, name='common git commands'),
+    path('basic-git-commands/', views.basicGitCommands, name='basic git commands'),
+    path('git-reference/', views.gitReference, name='git reference'),
 ]
