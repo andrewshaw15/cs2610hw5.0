b072b2dfd24426fec7f2dd6c466b4aedd53ce0dc
Andrew Shaw <35270950+andrewshaw15@users.noreply.github.com>
2018-03-12 19:43:58 -0600

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from time import strftime
 
@@ -10,3 +10,21 @@ def bio(request):
 
 def techTips(request):
     return render(request, 'blog/tech-tips.html', { 'current_time' : strftime('%c') })
+
+def starWars(request):
+    return redirect('http://www.starwars.com/')
+
+def airForce(request):
+    return redirect('http://www.af.mil/')
+
+def usu(request):
+    return redirect('https://www.usu.edu/')
+
+def commonGitCommands(request):
+    return redirect('http://guides.beanstalkapp.com/version-control/common-git-commands.html')
+
+def basicGitCommands(request):
+    return redirect('https://confluence.atlassian.com/bitbucketserver/basic-git-commands-776639767.html')
+
+def gitReference(request):
+    return redirect('https://git-scm.com/docs')
